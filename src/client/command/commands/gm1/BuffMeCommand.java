b05738f483cf1d31f7e470a6cdebabe334db480f
Conrad <kelvinngjunsheng@gmail.com>
2019-06-16 04:17:57 +0800

    Revert "Disable commands while in jail"
    This reverts commit f815b22c7efb4ea3650f46b9742dc518bbc78162.

--- src/client/command/commands/gm1/BuffMeCommand.java
+++ src/client/command/commands/gm1/BuffMeCommand.java
@@ -40,8 +40,7 @@ public class BuffMeCommand extends Command {
         SkillFactory.getSkill(2311003).getEffect(SkillFactory.getSkill(2311003).getMaxLevel()).applyTo(player);
         SkillFactory.getSkill(1301007).getEffect(SkillFactory.getSkill(1301007).getMaxLevel()).applyTo(player);
         SkillFactory.getSkill(2301004).getEffect(SkillFactory.getSkill(2301004).getMaxLevel()).applyTo(player);
-        SkillFactory.getSkill(3121002).getEffect(SkillFactory.getSkill(3121002).getMaxLevel()).applyTo(player);
         SkillFactory.getSkill(1005).getEffect(SkillFactory.getSkill(1005).getMaxLevel()).applyTo(player);
-        
+        player.healHpMp();
     }
 }
